1302d8ddf418ca81fba42fa8ac75917e53346253
vovisim_home <wova53209@yandex.ru>
2026-01-12 11:15:42 +0300

    master | Remove useMusicStore and hydrate function from MusicStore and update MusicCard component

--- app/components/Media/Music/index.vue
+++ app/components/Media/Music/index.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-const musicStore = useMusicStore();
+import type { IMusic } from "#shared/entities/Music";
 
-onMounted(() => {
-  musicStore.hydrate();
-});
+const musicStore = ref<IMusic[]>();
 </script>
 
 <template>
   <div class="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))]">
-    <MediaMusicCard
-      v-for="item in musicStore.items.toSpliced(0, 6)"
-      :key="item.id"
-      :music="item"
-    />
+    <MediaMusicCard v-for="item in musicStore" :key="item.id" :music="item" />
   </div>
 </template>
