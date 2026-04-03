2f68df9e92ba7d3b6b8a432cd3c4e95ba1260f92
vovisim_laptop <wova53209@yandex.ru>
2026-04-03 17:08:04 +0300

    Тайлер исчез

--- app/shared/constants/ProfileDropdownLinks.const.ts
+++ app/shared/constants/ProfileDropdownLinks.const.ts
@@ -1,7 +1,5 @@
 import type { IHeaderLink } from "#shared/interfaces/IHeaderLink";
-import { RoleEnum } from "#shared/enums/role.enum";
 
 export const ProfileDropdownLinksConst: IHeaderLink[] = [
   { label: "Профиль", to: { name: "profile" } },
-  { label: "Админ панель", to: { name: "adminMusic" }, role: RoleEnum.ADMIN },
 ] as const;
